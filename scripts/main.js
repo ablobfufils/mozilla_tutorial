ae0e7d322b0d0186432dfc29f0d3ce171a09a1ab
ablobfufils <coltermd@gmail.com>
2020-04-12 13:00:16 -0300

    added picture change javascript and new image josh-allen.png

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,11 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/josh-allen.png'){
+        myImage.setAttribute('src', 'images/josh-allen.png');
+    } else {
+        myImage.setAttribute ('src', 'images/josh-allen.png');
+    
+    }
+}
